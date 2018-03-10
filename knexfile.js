4e6e00c2e709e19808336b47c27d4d14d3ab65f9
seanhelvey <sean.helvey@gmail.com>
2018-03-09 17:02:19 -0700

    add production back to knex config

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/practice_dev'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/practice_dev'
   }
 
 };
