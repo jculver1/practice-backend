7d7246b19c9ba29e597f9fd0adbd10cf8cf6673c
seanhelvey <sean.helvey@gmail.com>
2018-03-09 20:35:40 -0700

    enable cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -10,6 +11,8 @@ var users = require('./routes/users');
 var shares = require('./routes/shares');
 
 var app = express();
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
